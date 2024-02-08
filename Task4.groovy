f4c6f7156a455a6d5821484cdc58a24622a084df
Yatish1045 <157652866+yatish1045@users.noreply.github.com>
2024-02-08 16:46:44 -0500

    Add files via upload

---
+++ Task4.groovy
@@ -0,0 +1,37 @@
+pipeline {
+    agent any
+    tools {
+        jdk 'JDK17'
+        maven 'maven3'
+    }
+
+    stages {
+        stage('Git Checkout') {
+            steps {
+                git branch: 'main', url: 'https://github.com/AnilkumarMaguluri18/Petclinic.git'
+            }
+        }
+        stage('Code Compile') {
+            steps {
+                bat label: 'Compile', script: 'mvn clean compile'
+            }
+        }
+        stage('Unit Tests') {
+            steps {
+                bat label: 'Run Tests', script: 'mvn test'
+            }
+        }
+       
+        stage('OWASP SCAN') {
+            steps {
+                dependencyCheck additionalArguments: ' --scan ./ ', odcInstallation: 'DP-Check'
+                dependencyCheckPublisher pattern: '**/dependency-check-report.xml'
+            }
+        }
+        stage('Build Artifact') {
+            steps {
+                bat label: 'Build Artifact', script: 'mvn clean install'
+            }
+        }
+    }
+}
